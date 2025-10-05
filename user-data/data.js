93a6730bf7b87e356bf76668ae48072be6cc93f4
Anders <anders-10@live.dk>
2025-10-05 21:59:10 +0200

    Initial commit, get most of the website to match my experiences.

--- user-data/data.js
+++ user-data/data.js
@@ -1,141 +1,143 @@
 const data ={
   "bio": [
-    "Hi, I'm Vinay Somawat — a Frontend Engineer with 5+ years of experience building scalable, high-performance web applications using Angular and React.",
-    "I specialize in component architecture, performance optimization, and modern frontend tooling like Storybook, Stencil, and Module Federation. I actively leverage GitHub Copilot and OpenAI APIs to accelerate UI development, automate workflows, and improve developer productivity.",
-    "I’m passionate about clean code, mentoring, and driving engineering excellence through collaboration, automation, and continuous improvement."
+    "Hi, I'm Anders Henriksen - I hold a Master’s degree in Artificial Intelligence and Data from DTU (Technical University of Denmark), with extensive Python experience gained at Novo Nordisk and Danske Bank.",
+    "I specialize in software engineering, design thinking, and top-down development - turning complex stakeholder requirements into clear, actionable tasks that can be efficiently distributed across teams.",
+    "I'm passionate about clean code, mentoring, and promoting engineering excellence through collaboration, automation, and continuous improvement.",
   ],
   "skills": [
-    "JavaScript",
-    "TypeScript",
-    "Angular",
-    "AngularJS",
-    "React",
-    "HTML5",
-    "CSS3",
-    "Tailwind CSS",
-    "Material UI",
-    "Bootstrap",
-    "RxJS",
-    "NGXS",
-    "NGRX",
-    "WebSockets",
-    "Module Federation",
-    "Signals API",
-    "Micro-frontend Architecture",
-    "StencilJS",
-    "Storybook",
-    "Webpack",
+    "Python",
+    "SQL",
     "Git",
-    "GitHub Copilot",
-    "OpenAI APIs",
-    "Chrome DevTools",
-    "Performance Optimization",
-    "Responsive Design",
-    "Web Accessibility",
-    "Unit Testing",
-    "ESLint",
-    "Prettier"
+    "AWS",
+    "Docker",
+    "CI/CD",
+    "Terraform",
+    "Version Controls Best Practices",
+    "Agile Methodologies",
+    "NumPy",
+    "Pandas",
+    "Jupyter Notebook",
+    "Linux",
+    "Bash",
+    "Scikit-Learn",
+    "Pytorch",
+    "MySQL",
+    "Microsoft SQL Server",
+    "vscode",
+    "Github Actions",
   ],
   "experience": [
+    // {
+    //   "title": "SS&C Technologies",
+    //   "duration": "August 2025 - Present",
+    //   "subtitle": "Senior Software Engineer",
+    //   "details": [
+    //     "coming soon..."
+    //   ],
+    //   "tags": [],
+    //   "icon": "money"
+    // },
     {
-      "title": "SS&C Technologies",
-      "duration": "August 2025 - Present",
-      "subtitle": "Senior Software Engineer",
+      "title": "Danske Bank",
+      "duration": "April 2025 - Present",
+      "subtitle": "Data Scientist",
       "details": [
-        "coming soon..."
+        "TODO."
       ],
-      "tags": [],
+      "tags": [
+        "TODO",
+      ],
       "icon": "money"
     },
-    {
-      "title": "Shiprocket Limited",
-      "duration": "September 2023 - August 2025",
-      "subtitle": "Software Engineer",
-      "details": [
-        "Led the migration of 30+ high-traffic modules (serving 300K+ active sellers) from AngularJS to Angular 19, conducting in-depth analysis of legacy code, identifying and addressing functional gaps in updated designs, executing testing across multiple user roles, and delivering a seamless rollout in a live production environment.",
-        "Engineered a ChatGPT-style courier rule engine using OpenAPI schema, automating 20+ ops tasks; built global search with deep linking and keyboard shortcuts, boosting efficiency by 30%",
-        "Contributed to the Shiprocket UI Design System using StencilJS + Storybook with atomic design, cutting UI bugs by 35% and halving feature delivery time across teams.",
-        "Integrated GitHub Copilot and ChatGPT into dev workflows, accelerating code generation and debugging; enforced linting pratices and mentored 5+ engineers in modern Angular."
-      ],
-      "tags": [
-        "TypeScript",
-        "Storybook",
-        "GitHub Copilot"
-      ],
-      "icon": "truck"
-    },
-    {
-      "title": "Biofourmis India Pvt. Ltd.",
-      "duration": "April 2022 - July 2023",
-      "subtitle": "Software Engineer",
-      "details": [
-        "Spearheaded micro-frontend adoption with Module Federation, eliminating shared deployment blockers and enabling team- wise independent releases.",
-        "Architected multi-region deployment support with dynamic API routing and failover, improving global latency.",
-        "Integrated real-time collaboration tools (Twilio Video, Countly, NGXS WebSockets) to enable 24/7 patient monitoring and synchronous team interactions."
-      ],
-      "tags": [
-        "TypeScript",
-        "RxJS",
-        "NGXS",
-        "WebSockets",
-        "Module Federation"
-      ],
-      "icon": "heartbeat"
-    },
-    {
-      "title": "Novopay Solutions Pvt. Ltd.",
-      "duration": "July 2020 - April 2022",
-      "subtitle": "Software Engineer",
-      "details": [
-        "Built Aadhaar-enabled banking modules (eKYC, withdrawal, balance inquiry) for 100K+ retail agents, ensuring secure financial access in rural areas.",
-        "Designed QR-code wallet flow, reducing transaction failures and increasing user adoption across merchants.",
-        "Integrated Clevertap, Freshdesk, and chatbot support, cutting average support resolution time by 40%."
-      ],
-      "tags": [
-        "JavaScript",
-        "Angular",
-        "Bootstrap"
-      ],
-      "icon": "qrcode"
-    }
+    // {
+    //   "title": "Shiprocket Limited",
+    //   "duration": "September 2023 - August 2025",
+    //   "subtitle": "Software Engineer",
+    //   "details": [
+    //     "Led the migration of 30+ high-traffic modules (serving 300K+ active sellers) from AngularJS to Angular 19, conducting in-depth analysis of legacy code, identifying and addressing functional gaps in updated designs, executing testing across multiple user roles, and delivering a seamless rollout in a live production environment.",
+    //     "Engineered a ChatGPT-style courier rule engine using OpenAPI schema, automating 20+ ops tasks; built global search with deep linking and keyboard shortcuts, boosting efficiency by 30%",
+    //     "Contributed to the Shiprocket UI Design System using StencilJS + Storybook with atomic design, cutting UI bugs by 35% and halving feature delivery time across teams.",
+    //     "Integrated GitHub Copilot and ChatGPT into dev workflows, accelerating code generation and debugging; enforced linting pratices and mentored 5+ engineers in modern Angular."
+    //   ],
+    //   "tags": [
+    //     "TypeScript",
+    //     "Storybook",
+    //     "GitHub Copilot"
+    //   ],
+    //   "icon": "truck"
+    // },
+    // {
+    //   "title": "Biofourmis India Pvt. Ltd.",
+    //   "duration": "April 2022 - July 2023",
+    //   "subtitle": "Software Engineer",
+    //   "details": [
+    //     "Spearheaded micro-frontend adoption with Module Federation, eliminating shared deployment blockers and enabling team- wise independent releases.",
+    //     "Architected multi-region deployment support with dynamic API routing and failover, improving global latency.",
+    //     "Integrated real-time collaboration tools (Twilio Video, Countly, NGXS WebSockets) to enable 24/7 patient monitoring and synchronous team interactions."
+    //   ],
+    //   "tags": [
+    //     "TypeScript",
+    //     "RxJS",
+    //     "NGXS",
+    //     "WebSockets",
+    //     "Module Federation"
+    //   ],
+    //   "icon": "heartbeat"
+    // },
+    // {
+    //   "title": "Novopay Solutions Pvt. Ltd.",
+    //   "duration": "July 2020 - April 2022",
+    //   "subtitle": "Software Engineer",
+    //   "details": [
+    //     "Built Aadhaar-enabled banking modules (eKYC, withdrawal, balance inquiry) for 100K+ retail agents, ensuring secure financial access in rural areas.",
+    //     "Designed QR-code wallet flow, reducing transaction failures and increasing user adoption across merchants.",
+    //     "Integrated Clevertap, Freshdesk, and chatbot support, cutting average support resolution time by 40%."
+    //   ],
+    //   "tags": [
+    //     "JavaScript",
+    //     "Angular",
+    //     "Bootstrap"
+    //   ],
+    //   "icon": "qrcode"
+    // }
   ],
   "education": [
     {
-      "title": "B.Tech. in Computer Science and Engineering",
-      "duration": "2016 - 2020",
-      "subtitle": "National Institute of Technology, Warangal",
+      "title": "MSc Human-centered Artificial Intelligence",
+      "duration": "2023 - 2024",
+      "subtitle": "Technical University of Denmark",
       "details": [],
       "tags": [],
       "icon": "graduation-cap"
     },
     {
-      "title": "Class 12th in Science and Mathematics",
-      "duration": "",
-      "subtitle": "Board of Secondary Education, Rajasthan",
+      "title": "BSc Artificial Intelligence and Data,",
+      "duration": "2018 - 2022",
+      "subtitle": "Technical University of Denmark",
       "details": [],
       "tags": [],
-      "icon": "book"
-    }
+      "icon": "graduation-cap"
+    },
   ],
   "contactLinks": [
     {
       "label": "Email",
-      "link": "mailto:vinaysomawat40@gmail.com",
+      "link": "mailto:anders-10@live.dk",
       "icon": "fa fa-envelope"
     },
     {
       "label": "LinkedIn",
-      "link": "https://www.linkedin.com/in/vinaysomawat/",
+      "link": "https://www.linkedin.com/in/andershenriksen/",
       "icon": "fa-brands fa-linkedin"
     },
     {
       "label": "GitHub",
-      "link": "https://github.com/vinaysomawat",
+      "link": "https://github.com/AndersHenriksen-dev",
       "icon": "fa-brands fa-github"
     },
     {
       "label": "Buy me a coffee",
-      "link": "https://www.buymeacoffee.com/r194dME8y",
+      "link": "https://buymeacoffee.com/andershenriksen",
       "icon": "fa fa-coffee"
     }
   ],
@@ -145,11 +147,11 @@ const data ={
       "data": [
         {
           "text": "Buy me a coffee",
-          "link": "https://www.buymeacoffee.com/r194dME8y"
+          "link": "https://buymeacoffee.com/andershenriksen"
         },
         {
           "text": "Github",
-          "link": "https://github.com/vinaysomawat/vinaysomawat.github.io"
+          "link": "https://andershenriksen-dev.github.io/portfolio/"
         }
       ]
     },
@@ -169,7 +171,7 @@ const data ={
     {
       "label": "copyright-text",
       "data": [
-        "Built with HTML, CSS, JavaScript, and ❤️"
+        "Built with HTML, CSS and JavaScript"
       ]
     }
   ]
